319d0a1e3acfb185dde8668c6e9e87ef53ae7c94
Aluno <aluno@etec.sp.gov.br>
2023-11-06 20:45:22 -0300

    Atividade Aula 06/11

--- PWFilmes.Domain/Categoria.cs
+++ PWFilmes.Domain/Categoria.cs
@@ -21,7 +21,7 @@ namespace PWFilmes.Domain
         [MaxLength(100, ErrorMessage = "A Descrição deve conter no máximo 100 caracteres.")]
         public string Descricao { get; set; }
         
-        [Required(ErrorMessage = "A Cir deve ser informada.")]
+        [Required(ErrorMessage = "A Cor deve ser informada.")]
         [MaxLength(50, ErrorMessage = "A Cor deve conter no máximo 50 caracteres.")]
         public string Cor { get; set; }
     }
